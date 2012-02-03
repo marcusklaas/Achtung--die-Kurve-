8da600f166cbc89364013f7248b6b01cb2ac8acf
Marcus Klaas de Vries <marcusdevries@gmail.com>
2012-02-03 20:29:11 +0100

    paar kleine todos

--- client/config.js
+++ client/config.js
@@ -23,7 +23,7 @@ var indicatorArrowOffset = 2;
 var syncTries = 2;
 var syncDelays = 50;
 var tickTockDifference = 2; // desired difference between tock and tick
-var canvasMinimumWidth = 200;
+var canvasMinimumWidth = 300;
 var crossSize = 12;
 var crossLineWidth = 2;
 var crossColor = [0, 0, 0];
@@ -56,11 +56,11 @@ var pencilAlpha = 0.2;
 /* debugging */
 var emulateTouch = false;
 var ultraVerbose = false;
-var simulatedPing = 200;
+var simulatedPing = 0;
 var extraGameStartTimeDifference = 0;
 var acceptGameTimeAdjustments = true;
 var jsProfiling = false;
-var simulateCPUlag = true;
+var simulateCPUlag = false;
 var debugBaseContext = false; // sets different color for segments in basecontext
 var displayDebugStatus = false;
 var debugRewards = false;
@@ -75,8 +75,7 @@ var modeJson = 3;
 var editorStepTime = 100;
 var mapSegmentColor = [96, 96, 96];
 
-/* these are the segment RGBs for the players. in client order, so the 
- * first one is always for the human player */
+/* these are the segment RGBs for the players */
 var playerColors = [[255, 0, 0], [0, 0, 255], [0, 255, 0],
  [255, 0, 255], [229, 91, 176], [78, 42, 4], [28, 182, 25], [126, 191, 241],
  [0, 0, 0], [254, 138, 14], [149, 150, 151], [16, 98, 70], [127, 200, 0]];
